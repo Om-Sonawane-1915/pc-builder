22787f9239e95c16cf144f2348351c073e8fac8c
Om Sonawane <omsonawane1915@gmail.com>
2026-07-13 17:09:02 +0530

    Add CPU endpoint

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,15 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Welcome to PC Builder API"}
+
+@app.get("/cpus")
+def get_cpus():
+    return [
+        {
+            "id": 1,
+            "name": "AMD Ryzen 5 7600",
+            "cores": 6,
+            "threads": 12,
+            "price": 18999
+        }
+    ]
